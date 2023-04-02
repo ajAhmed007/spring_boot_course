63ad2f148808080b1660221920465ba7b959d44e
CSS432Ahmed <abdula88@uw.edu>
2023-04-01 23:25:23 -0700

    initial

---
+++ spring_boot_project/src/main/java/com/ahmed/spring_boot_project/Main.java
@@ -0,0 +1,19 @@
+package com.ahmed.spring_boot_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello(){
+		return "Hello";
+	}
+
+}
